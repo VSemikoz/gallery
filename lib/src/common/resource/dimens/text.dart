bf36f712f3876fd5ba2d25bfdd65dc8d8074af22
vsemikoz <semikoz@miura.tech>
2022-01-16 18:30:35 +0300

    Add TextResources

---
+++ lib/src/common/resource/dimens/text.dart
@@ -0,0 +1,5 @@
+class TextResources {
+  static const String homeAppBar = "Gallery";
+  static const String homeBottomEnd = "end of story :(";
+  static const String homeErrorLoading = "error loading";
+}
